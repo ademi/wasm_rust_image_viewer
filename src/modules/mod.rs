312611d11a3e88d673e016e66a3da3d3607a072c
ademi <ademi>
2020-01-04 10:56:10 +1100

    Create gh-pages:

--- src/modules/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod buffer;
-pub use self::buffer::Buffer;
-
-pub mod frame;
-pub use self::frame::Frame;
-
-//pub mod pixel;
-//pub use self::pixel::Pixel;
--- src/modules/pixel.rs
+++
@@ -1,9 +0,0 @@
-pub struct Pixel{
-    r:u8,
-    g:u8,
-    b:u8,
-    a:u8, 
-}
-impl Pixel{
-    
-}
